de8ad1da8d63fd0fff5b8f78a3165bc2c16c08a9
contactkkiran <contactkkiran@gmail.com>
2026-08-24 06:21:26 +0530

    avation stackadded

--- src/config/settings.py
+++ src/config/settings.py
@@ -4,5 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+AVIATIONSTACK_API_KEY = os.getenv("AVIATIONSTACK_API_KEY")
+
 if not ANTHROPIC_API_KEY:
     raise ValueError("ANTHROPIC_API_KEY is not set in the environment variables.")
+if not AVIATIONSTACK_API_KEY:
+    raise ValueError("AVIATIONSTACK_API_KEY is not set in the environment variables.")
